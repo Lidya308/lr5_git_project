56ed0f01a2d5add5828abd197175412b1793b203
лидия <lida.tupikova@yandex.ru>
2025-11-10 19:40:00 +0300

    Add new calculation function and improve main logic

--- main.py
+++ main.py
@@ -1,16 +1,9 @@
-# This is a sample Python script.
+def new_calculation_function(a, b):
+    """Новая функция для демонстрации Git"""
+    return a * b + 10
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# Измените основной код
+if __name__ == "__main__":
+    print("Добавлена новая функциональность!")
+    result = new_calculation_function(5, 3)
+    print(f"Результат: {result}")
